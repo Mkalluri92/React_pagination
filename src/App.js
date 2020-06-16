033f64fe785f97fbe6cfd535a03d239a273ba696
mkalluri92 <mkalluri92@gmail.com>
2020-06-15 18:30:45 -0700

    created a table to show data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+class App extends Component {
+
+  state = {
+    details: null
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:8081`)
+    .then(response => {
+      this.setState({details: response})
+    })
+  }
+
+  render () {
+    let personDetails = null;
+    if(this.state.details) {
+      personDetails = this.state.details.data.data.map(current => {
+        return <tr key={current.firstName}>
+          <td>{current.firstName}</td>
+          <td>{current.lastName}</td>
+          <td>{current.country}</td>
+          <td>{current.age}</td>
+        </tr>
+      })
+    }
+    return (
+      this.state.details? <table>
+        <thead>
+          <tr>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Country</td>
+            <td>Age</td>
+          </tr>
+        </thead>
+        <tbody>
+            {personDetails}
+        </tbody>
+      </table>: null
+    )
+  }
 }
 
 export default App;
